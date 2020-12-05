dbcc7b19781364f38c89b8ef65bbb5fd5708dc44
rextejiri <rextejiri@gmail.com>
2020-12-04 21:14:55 -0600

    deciding what datato display

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 $(() => {
-
+  let feels_like = $('.feels_like')
   $.ajax({
       url:`http://api.openweathermap.org/data/2.5/weather?zip=75249&appid=530e784297febf79825bcc72dc838534`
   }).then(
     (data) => {
-    console.log(data);
+      // const weather = $(data.weather)
+      // $('.city').html(data.main.pressure)
+      // for (let kkk of weather) {
+      //   feels_like.html(kkk.description)
+      //   feels_like.css('color', 'red')
+      //   console.log(data.main);
+      }
+
   },(erro) => {
 
   })
