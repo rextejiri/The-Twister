55582346416cb50b1c7180aae0842cb4fe276af3
rextejiri <rextejiri@gmail.com>
2020-12-05 10:39:38 -0600

    html solid

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 $(() => {
   let feels_like = $('.feels_like')
   $.ajax({
-      url:`http://api.openweathermap.org/data/2.5/weather?zip=75249&appid=530e784297febf79825bcc72dc838534`
+      url:`http://api.openweathermap.org/data/2.5/weather?zip=76001&appid=530e784297febf79825bcc72dc838534`
   }).then(
     (data) => {
-      // const weather = $(data.weather)
-      // $('.city').html(data.main.pressure)
-      // for (let kkk of weather) {
-      //   feels_like.html(kkk.description)
-      //   feels_like.css('color', 'red')
-      //   console.log(data.main);
-      }
+      console.log(data.weather);
+    },
+    (error) => {
 
-  },(erro) => {
-
-  })
-
-
-
-
-
+    }
+  )
 
 })
